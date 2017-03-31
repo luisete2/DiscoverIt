8608a66713b7c12d185c97ac6af1d9e8059633ef
robertoplaza <robertoplaza@playmuser.com>
2017-03-31 10:23:44 +0200

    Coordenadas

--- www/php/di_serverCamera.php
+++ www/php/di_serverCamera.php
@@ -9,6 +9,32 @@
     ));
     //move_uploaded_file($_FILES["file"]["tmp_name"], '/path/to/file');
     $imagen = $_REQUEST['imagen'];
+    
+         $exif = exif_read_data($imagen);
+            $longitud = getGps($exif["GPSLongitude"], $exif['GPSLongitudeRef']);
+            $latitud = getGps($exif["GPSLatitude"], $exif['GPSLatitudeRef']);
+            
+            echo "<br><br>La latitud de la imagen es: " . $latitud . "<br>";
+            echo "La longitud de la imagen es: " . $longitud . "<br>";
+
+            function getGps($exifCoord, $hemi){
+                $degrees = count($exifCoord) > 0 ? gps2Num($exifCoord[0]) : 0;
+                $minutes = count($exifCoord) > 1 ? gps2Num($exifCoord[1]) : 0;
+                $seconds = count($exifCoord) > 2 ? gps2Num($exifCoord[2]) : 0;
+                $flip = ($hemi == 'W' or $hemi == 'S') ? -1 : 1;
+                return $flip * ($degrees + $minutes / 60 + $seconds / 3600);
+            }
+
+            function gps2Num($coordPart) {
+                $parts = explode('/', $coordPart);
+                if(count($parts) <= 0){
+                    return 0;
+                }
+                if (count($parts) == 1){
+                    return $parts[0];
+                }                
+                return floatval($parts[0]) / floatval($parts[1]);
+            }
     //\Cloudinary\Uploader::upload($_FILES["file"]["tmp_name"]);
     echo $imagen;
 ?>
